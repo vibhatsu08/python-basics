e8181b27133406f32216f07f804b8c30e3c8a615
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-27 02:10:04 +0530

    swap two variable values
    python program to swap the values between two variables.

---
+++ python6.py
@@ -0,0 +1,13 @@
+#python program to swap the values of two given variables
+num1 = input()
+num2 = input()
+num1 = int(num1)
+num2 = int(num2)
+print(num1)
+print(num2)
+
+temp = num1 
+num1 = num2
+num2 = temp
+print(num1)
+print(num2)
