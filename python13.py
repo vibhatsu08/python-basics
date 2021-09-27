04178d254ad1f63d67a1add72fb61e74dc4b9f65
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-27 12:41:28 +0530

    armstrong number
    python program to check if a given number is an Armstrong number or not.

---
+++ python13.py
@@ -0,0 +1,24 @@
+#python program to check if a given number is an armstrong number or not.
+number = int(input())
+order = len(str(number))
+
+temp = number
+sum = 0
+
+while temp > 0:
+    lastDigit = temp % 10
+    sum += lastDigit ** order
+    temp //= 10
+    
+print(sum)
+
+if number == sum:
+    print("given number is an armstrong number")
+else:
+    print("given number is not an armstrong number")
+
+
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
