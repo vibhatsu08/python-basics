899d15bc76f0ade3f0dbf0a006a5a1a468042961
vibhatsu08 <mistryvedant17@gmail.com>
2023-02-15 18:26:28 +0530

    Program to implement Selection sort in Python.

--- selectionSort.py
+++ selectionSort.py
@@ -4,8 +4,8 @@ def selection_sort (nums) :
     for i in range (len(nums)) :
         minIndex = i
         for j in range (i+1, len(nums)) :
-            if nums[j] < nums[minIndex] :
+            if nums[minIndex] > nums[j] :
                 minIndex = j
-        nums[i], nums[minIndex] = nums[minIndex], nums[i]
+        nums[minIndex], nums[i] = nums[i], nums[minIndex]
     return nums
 print(selection_sort(nums = [3,5,6,8,6,1,9,4,2,10,6,7,4,6,17,3]))
